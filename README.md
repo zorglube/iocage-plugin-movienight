d6a2d08e9e22089932354d833a8cbc563996d70b
Zorglube <zorglube@broncotoxique.com>
2020-08-16 20:18:38 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# freenas-iocage-movienight
+This is a simple script to automate the installation of MovieNight in a FreeNAS jail. It will create a jail, download 
+and install GO from [Golang](https://golang.org/dl/) and download compile and install the latest version of MovieNight from [MovieNight](https://github.com/zorchenhimer/MovieNight). 
+
+## Status
+This script has been built under FreeNAS 11.3 U4.1 with FreeBSD 11.3-Release-P11. It 
+should work for FreeNAS 11.3, and it should also work with TrueNAS CORE 12.0. Feel free 
+to report any bad operation or purpose any improvement.
+
+## Usage
+Since the COVID-19 has locked down everyone at home, we couldn't go to movie theater 
+together. MovieNight is an solution to overcome that. Quoting the MovieNight developer : 
+"This is a single-instance streaming server with chat. [...] platform for watching movies with a group of people online." If 
+you wonder how to use after installation, have an look into the MovieNight manual [MovieNight](https://github.com/zorchenhimer/MovieNight). 
+
+### Prerequisites
+MovieNight don't need to access any data outside of the jail. At this point, since MN 
+need an GO runtime, the runtime will be stored into the jail. 
+
+### Installation
+Download the repository to a convenient directory on your FreeNAS system by changing to that directory and running `git clone https://github.com/zorglube/freenas-iocage-movienight`. Then change into the new freenas-iocage-movienight directory and create a file called `mn-config` with your favorite text editor. In its minimal form, it would look like this:
+
+```
+JAIL_IP="10.1.1.3"
+DEFAULT_GW_IP="10.1.1.1"
+```
+
+Many of the options are self-explanatory, and all should be adjusted to suit your needs, but only a few are mandatory. The mandatory options are:
+
+- JAIL_IP is the IP address for your jail. You can optionally add the netmask in CIDR notation (e.g., 192.168.1.199/24). If not specified, the netmask defaults to 24 bits. Values of less than 8 bits or more than 30 bits are invalid.
+- DEFAULT_GW_IP is the address for your default gateway
+- POOL_PATH is the path for your data pool.
+
+In addition, there are some other options which have sensible defaults, but can be adjusted if needed. These are:
+
+- JAIL_NAME: The name of the jail, defaults to `rslsync`
+- CONFIG_PATH: Client configuration data is stored in this path; defaults to `$POOL_PATH/apps/rslsync/config`.
+- DATA_PATH: Selective backups are stored in this path; defaults to `$POOL_PATH/apps/rslsync/data`.
+- INTERFACE: The network interface to use for the jail. Defaults to `vnet0`.
+- VNET: Whether to use the iocage virtual network stack. Defaults to `on`.
+
+### Execution
+
+Once you've downloaded the script and prepared the configuration file, run this script (`./rslsync-jail.sh`). The script will run for several minutes. When it finishes, your jail will be created and rslsync will be installed.
+
+### Test
+
+To test your installation, enter your Resilio Sync jail IP address and port 8888 e.g. `10.1.1.3:8888` in a browser. If the installation was successful, you should see a Resilio Sync configuration screen.
+
+### Initial Configuration
+
+`$DATA_PATH` is mounted inside the jail at `/media`. Your backups go there. When configuring the application, point the `Default folder location` and `File download location` to `/media` in the preference settings.
+
+## Support and Discussion
+
+Useful sources of support include the [Sync Help Centre](https://help.resilio.com/hc/en-us/categories/200140177-Get-started-with-Sync) and [Sync Forum](https://forum.resilio.com/)
+
+Questions or issues about this resource can be raised in [this forum thread](https://www.ixsystems.com/community/threads/scripted-resilio-sync-installation.86766/).  
+
+
---
+++ movienight-jail.sh
@@ -0,0 +1,183 @@
+#!/bin/sh
+# Build an iocage jail under FreeNAS 11.3-12.0 using the current release of Resilio Sync
+# git clone https://github.com/basilhendroff/freenas-iocage-rslsync
+
+# Check for root privileges
+if ! [ $(id -u) = 0 ]; then
+   echo "This script must be run with root privileges"
+   exit 1
+fi
+
+#####
+#
+# General configuration
+#
+#####
+
+# Initialize defaults
+JAIL_IP=""
+JAIL_INTERFACES=""
+DEFAULT_GW_IP=""
+INTERFACE="vnet0"
+VNET="on"
+POOL_PATH=""
+JAIL_NAME="rslsync"
+CONFIG_NAME="rslsync-config"
+CONFIG_PATH=""
+DATA_PATH=""
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "${SCRIPT}")
+if ! [ -e "${SCRIPTPATH}"/"${CONFIG_NAME}" ]; then
+  echo "${SCRIPTPATH}/${CONFIG_NAME} must exist."
+  exit 1
+fi
+. "${SCRIPTPATH}"/"${CONFIG_NAME}"
+INCLUDES_PATH="${SCRIPTPATH}"/includes
+
+JAILS_MOUNT=$(zfs get -H -o value mountpoint $(iocage get -p)/iocage)
+RELEASE=$(freebsd-version | sed "s/STABLE/RELEASE/g" | sed "s/-p[0-9]*//")
+
+# Check for rslsync-config and set configuration
+if ! [ -e "${SCRIPTPATH}"/rslsync-config ]; then
+  echo "${SCRIPTPATH}/rslsync-config must exist."
+  exit 1
+fi
+
+#####
+#
+# Input/Config Sanity checks
+#
+#####
+
+# Check that necessary variables were set by nextcloud-config
+if [ -z "${JAIL_IP}" ]; then
+  echo 'Configuration error: JAIL_IP must be set'
+  exit 1
+fi
+if [ -z "${JAIL_INTERFACES}" ]; then
+  echo 'JAIL_INTERFACES not set, defaulting to: vnet0:bridge0'
+  JAIL_INTERFACES="vnet0:bridge0"
+fi
+if [ -z "${DEFAULT_GW_IP}" ]; then
+  echo 'Configuration error: DEFAULT_GW_IP must be set'
+  exit 1
+fi
+if [ -z "${POOL_PATH}" ]; then
+  echo 'Configuration error: POOL_PATH must be set'
+  exit 1
+fi
+
+# If DATA_PATH and CONFIG_PATH weren't set in rslsync-config, set them
+if [ -z "${DATA_PATH}" ]; then
+  DATA_PATH="${POOL_PATH}"/apps/rslsync/data
+fi
+if [ -z "${CONFIG_PATH}" ]; then
+  CONFIG_PATH="${POOL_PATH}"/apps/rslsync/config
+fi
+
+# Sanity check DATA_PATH and CONFIG_PATH -- they have to be different and can't be the same as POOL_PATH
+if [ "${CONFIG_PATH}" = "${DATA_PATH}" ]
+then
+  echo "CONFIG_PATH and DATA_PATH must be different!"
+  exit 1
+fi
+if [ "${DATA_PATH}" = "${POOL_PATH}" ] || [ "${CONFIG_PATH}" = "${POOL_PATH}" ]
+then
+  echo "FILES_PATH and CONFIG_PATH must all be different from POOL_PATH!"
+  exit 1
+fi
+
+# Extract IP and netmask, sanity check netmask
+IP=$(echo ${JAIL_IP} | cut -f1 -d/)
+NETMASK=$(echo ${JAIL_IP} | cut -f2 -d/)
+if [ "${NETMASK}" = "${IP}" ]
+then
+  NETMASK="24"
+fi
+if [ "${NETMASK}" -lt 8 ] || [ "${NETMASK}" -gt 30 ]
+then
+  NETMASK="24"
+fi
+
+#####
+#
+# Jail Creation
+#
+#####
+
+# List packages to be auto-installed after jail creation
+cat <<__EOF__ >/tmp/pkg.json
+	{
+  "pkgs":[
+  "nano","bash","ca_root_nss"
+  ]
+}
+__EOF__
+
+# Create the jail and install previously listed packages
+if ! iocage create --name "${JAIL_NAME}" -p /tmp/pkg.json -r "${RELEASE}" interfaces="${JAIL_INTERFACES}" ip4_addr="${INTERFACE}|${JAIL_IP}/24" defaultrouter="${DEFAULT_GW_IP}" boot="on" host_hostname="${JAIL_NAME}" vnet="${VNET}"
+then
+	echo "Failed to create jail"
+	exit 1
+fi
+rm /tmp/pkg.json
+
+#####
+#
+# Directory Creation and Mounting
+#
+#####
+
+mkdir -p "${CONFIG_PATH}"
+mkdir -p "${DATA_PATH}"
+chown -R 817:817 "${CONFIG_PATH}" "${DATA_PATH}"
+
+iocage exec "${JAIL_NAME}" mkdir -p /tmp/includes
+iocage exec "${JAIL_NAME}" mkdir -p /var/db/rslsync
+iocage exec "${JAIL_NAME}" mkdir -p /usr/local/etc/rc.d
+iocage exec "${JAIL_NAME}" mkdir -p /usr/local/bin
+
+iocage exec "${JAIL_NAME}" "pw user add rslsync -c rslsync -u 817 -d /nonexistent -s /usr/bin/nologin"
+
+iocage fstab -a "${JAIL_NAME}" "${INCLUDES_PATH}" /tmp/includes nullfs rw 0 0
+iocage fstab -a "${JAIL_NAME}" "${CONFIG_PATH}" /var/db/rslsync nullfs rw 0 0
+iocage fstab -a "${JAIL_NAME}" "${DATA_PATH}" /media nullfs rw 0 0
+
+#####
+#
+# Rslsync Download and Setup
+#
+#####
+
+FILE="resilio-sync_freebsd_x64.tar.gz"
+if ! iocage exec "${JAIL_NAME}" fetch -o /tmp https://download-cdn.resilio.com/stable/FreeBSD-x64/"${FILE}"
+then
+	echo "Failed to download Resilio/Sync"
+	exit 1
+fi
+if ! iocage exec "${JAIL_NAME}" tar xzf /tmp/"${FILE}" -C /usr/local/bin/
+then
+	echo "Failed to extract Resilio/Sync"
+	exit 1
+fi
+iocage exec "${JAIL_NAME}" rm /tmp/"${FILE}"
+
+# Copy pre-written config files
+iocage exec "${JAIL_NAME}" cp /tmp/includes/rslsync /usr/local/etc/rc.d/
+iocage exec "${JAIL_NAME}" cp /tmp/includes/rslsync.conf.sample /usr/local/etc/
+iocage exec "${JAIL_NAME}" cp /tmp/includes/rslsync.conf.sample /usr/local/etc/rslsync.conf
+
+iocage exec "${JAIL_NAME}" sysrc rslsync_enable="YES"
+
+iocage restart "${JAIL_NAME}"
+
+# Don't need /mnt/includes any more, so unmount it
+iocage fstab -r "${JAIL_NAME}" "${INCLUDES_PATH}" /tmp/includes nullfs rw 0 0
+iocage exec "${JAIL_NAME}" rmdir /tmp/includes
+
+
+
+
+
+
