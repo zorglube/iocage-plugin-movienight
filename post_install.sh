0e37cbfd4bd0c4a538910faa8ab608f5f166e419
Zorglube <630192+zorglube@users.noreply.github.com>
2021-02-15 01:14:40 +0100

    Reopen the script

--- post_install.sh
+++ post_install.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 # This script is the post install of the MovieNight FreeNAS plugin. 
 # It's main purpose is to download GoLang SDK and MovieNight sources, then it build MovieNight and run it.   
 # git clone https://github.com/zorglube/freenas-iocage-movienight
@@ -24,10 +23,10 @@
 #
 #####
 # UID:GID is 'movien' because 'movienight' is to long
-#UID=movien
-#GID=${UID}
-#UID_GID_ID=850
-#pw user add ${UID} -c ${GID} -u ${UID_GID_ID} -d /nonexistent -s /usr/bin/nologin
+UID=movien
+GID=${UID}
+UID_GID_ID=850
+pw user add ${UID} -c ${GID} -u ${UID_GID_ID} -d /nonexistent -s /usr/bin/nologin
 
 #####
 #
@@ -39,18 +38,18 @@ ARCH=amd64
 MN_REPO=https://github.com/zorglube/MovieNight.git
 MN_HOME=/usr/local/movienight
 MN_MAKEFILE=${MN_HOME}/Makefile.BSD
-#mkdir ${MN_HOME}
-#git clone ${MN_REPO} ${MN_HOME} --verbose
-#make TARGET=${TARGET} ARCH=${ARCH} -f ${MN_MAKEFILE} -C ${MN_HOME}
-#chown -R ${UID}:${GID} ${MN_HOME}
+mkdir ${MN_HOME}
+git clone ${MN_REPO} ${MN_HOME} --verbose
+make TARGET=${TARGET} ARCH=${ARCH} -f ${MN_MAKEFILE} -C ${MN_HOME}
+chown -R ${UID}:${GID} ${MN_HOME}
 # Set the MovieNight config
-#rm ${MN_HOME}/settings.json
-#mv ${MN_HOME}/settings-freebsd.json ${MN_HOME}/settings.json
-#chmod 755 ${MN_HOME}
-#chmod +x ${MN_HOME}/MovieNight
+rm ${MN_HOME}/settings.json
+mv ${MN_HOME}/settings-freebsd.json ${MN_HOME}/settings.json
+chmod 755 ${MN_HOME}
+chmod +x ${MN_HOME}/MovieNight
 # Create MN log file
-#touch /var/log/movienight.log
+touch /var/log/movienight.log
 # Enable the service
-#sysrc -f /etc/rc.conf movienight_enable="YES"
+sysrc -f /etc/rc.conf movienight_enable="YES"
 # Start the service
-#service movienight start
+service movienight start
