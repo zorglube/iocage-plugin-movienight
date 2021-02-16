b203131f94b34b670fca9701e9a05ab5c4bf42e1
Zorglube <zorglube@broncotoxique.com>
2021-02-16 22:19:29 +0100

    Remove shell -x

--- post_install.sh
+++ post_install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh +x
+#!/bin/sh
 # This script is the post install of the MovieNight FreeNAS plugin.
 # It's main purpose is to download GoLang SDK and MovieNight sources, then it build MovieNight and run it.
 # git clone https://github.com/zorglube/freenas-iocage-movienight
@@ -24,21 +24,32 @@ ARCH=amd64
 MN_REPO=https://github.com/zorglube/MovieNight.git
 MN_HOME=/usr/local/movienight
 MN_MAKEFILE=${MN_HOME}/Makefile.BSD
+
 mkdir ${MN_HOME}
+
 echo "=> Git Clonning "
 git clone ${MN_REPO} ${MN_HOME} --verbose && echo "=> Cloned "
+
 echo "=> Building "
 make TARGET=${TARGET} ARCH=${ARCH} -f ${MN_MAKEFILE} -C ${MN_HOME} && echo " Build end "
+
+while [ ! -f ./MovieNight ]; do sleep 5; done
+
 read -p "press any key..."
+
 chown -R ${UID}:${GID} ${MN_HOME}
+
 # Set the MovieNight config
 rm ${MN_HOME}/settings.json
 mv ${MN_HOME}/settings-freebsd.json ${MN_HOME}/settings.json
 chmod 755 ${MN_HOME}
 chmod +x ${MN_HOME}/MovieNight
+
 # Create MN log file
 touch /var/log/movienight.log
+
 # Enable the service
 sysrc -f /etc/rc.conf movienight_enable="YES"
+
 # Start the service
 service movienight start 2>/dev/null
