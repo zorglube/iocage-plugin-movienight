8061c6fe2a4fbb79984f93a72492c48214f7bf6d
Zorglube <630192+zorglube@users.noreply.github.com>
2021-02-17 01:10:33 +0100

    Commit bin...

--- post_install.sh
+++ post_install.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-wget https://github.com/zorglube/MovieNight/releases/download/2021-02-16/MovieNight -P /usr/local/movienight
-
 chmod +x /usr/local/movienight/MovieNight
 
 # Create MN log file
