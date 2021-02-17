d816da115cba72b8d5ba2bafe6016feb18a7d59b
Zorglube <630192+zorglube@users.noreply.github.com>
2021-02-17 01:01:25 +0100

    Try to reduce more

--- post_install.sh
+++ post_install.sh
@@ -1,17 +1,10 @@
 #!/bin/sh
 
-MN_HOME=/usr/local/movienight
-
 wget https://github.com/zorglube/MovieNight/releases/download/2021-02-16/MovieNight -P ${MN_HOME} --verbose
 
 chmod +x ${MN_HOME}/MovieNight
 
-# Create MN log file
-touch /var/log/movienight.log
-
-# Enable the service
 sysrc -f /etc/rc.conf movienight_enable="YES"
 
 # Start the service
-service movienight status
 service movienight start 2>/dev/null
