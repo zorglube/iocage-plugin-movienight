8bcb2fd586b0890617770be0eca72fe653a055ea
Zorglube <630192+zorglube@users.noreply.github.com>
2021-02-17 01:06:35 +0100

    Fix error

--- post_install.sh
+++ post_install.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-wget https://github.com/zorglube/MovieNight/releases/download/2021-02-16/MovieNight -P ${MN_HOME} --verbose
+wget https://github.com/zorglube/MovieNight/releases/download/2021-02-16/MovieNight -P /usr/local/movienight --verbose
 
-chmod +x ${MN_HOME}/MovieNight
+chmod +x /usr/local/movienight/MovieNight
 
+# Create MN log file
+
+# Enable the service
 sysrc -f /etc/rc.conf movienight_enable="YES"
 
 # Start the service
+service movienight status
 service movienight start 2>/dev/null
